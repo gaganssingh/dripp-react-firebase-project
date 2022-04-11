60eb26dc74fbd3fb4212555ea8cdefb33f09576d
Gagan Singh <gagansingh.dev@outlook.com>
2022-04-11 17:28:25 -0400

    Implemented basic auth state using Context

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { UserProvider } from "./contexts/user.context";
 import "./index.scss";
 
 const entryPoint = document.getElementById("root");
 createRoot(entryPoint).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
