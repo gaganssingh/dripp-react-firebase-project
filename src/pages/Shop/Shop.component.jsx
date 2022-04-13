4471159b913858aa945b32ebc055638251a7398d
Gagan Singh <gagansingh.dev@outlook.com>
2022-04-12 23:44:18 -0400

    Added category specific pages and implemented router handling

--- src/pages/Shop/Shop.component.jsx
+++ src/pages/Shop/Shop.component.jsx
@@ -1,24 +1,14 @@
-import { Fragment, useContext } from "react";
-import ProductCard from "../../components/ProductCard/ProductCard.component";
-import { CategoriesContext } from "../../contexts/categories.context";
+import { Route, Routes } from "react-router-dom";
+import CategoriesPreview from "../CategoriesPreview/CategoriesPreview.component";
+import Category from "../Category/Category.component";
 import "./Shop.styles.scss";
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
-
   return (
-    <>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2>{title}</h2>
-          <div className="products-container">
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </Fragment>
-      ))}
-    </>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
   );
 };
 
