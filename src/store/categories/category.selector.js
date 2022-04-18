a836dbdd9cd4f02268de5b323bc1b9b00e88f4e5
Gagan Singh <gagansingh.dev@outlook.com>
2022-04-18 00:17:41 -0400

    Refactored the category selector to used memoized/cached data via the reselect library

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,8 +1,22 @@
-export const selectCategoriesMap = (state) => {
-  console.log("Category selector fired");
-  return state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
-};
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  // Memoized selector
+  // As long as state of data inside the categories collection is the same
+  // caches the value of the .reduce on the categories collection
+  // if (selectCategories deep-equal the prev selectCategories), dont change
+  [selectCategories],
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
